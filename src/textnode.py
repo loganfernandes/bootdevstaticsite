398b996cc0d8cd77ac0c7a930ac6c1f9d0561644
Logan Fernandes <logan.j.fernandes@gmail.com>
2026-04-19 20:59:23 +0000

    moved functions around

--- src/textnode.py
+++ src/textnode.py
@@ -36,22 +36,3 @@ def text_node_to_html_node(text_node):
     if text_node.text_type == TextType.IMAGE:
         return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
     
-def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    new_nodes = []
-    for old_node in old_nodes:
-        if old_node.text_type != TextType.TEXT:
-            new_nodes.append(old_node)
-            continue
-        split_nodes = []
-        sections = old_node.text.split(delimiter)
-        if len(sections) % 2 == 0:
-            raise ValueError("invalid markdown, formatted section not closed")
-        for i, section in enumerate(sections):
-            if section == "":
-                continue
-            if i % 2 == 0:
-                split_nodes.append(TextNode(section, TextType.TEXT))
-            else:
-                split_nodes.append(TextNode(section, text_type))
-        new_nodes.extend(split_nodes)
-    return new_nodes
